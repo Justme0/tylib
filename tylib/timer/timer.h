286a417f1d82fa5123a055a931973012db8185c9
Justme0 <jiangg2012@hotmail.com>
2022-10-09 15:39:15 +0800

    Add timer utility

---
+++ tylib/timer/timer.h
@@ -0,0 +1,131 @@
+#ifndef TYLIB_TIMER_TIMER_H_
+#define TYLIB_TIMER_TIMER_H_
+
+#include <sys/time.h>
+
+#include <atomic>
+#include <cstdint>
+#include <ctime>
+#include <mutex>
+#include <vector>
+
+class Time {
+ public:
+  Time();
+  Time(const Time& other);
+  // OPT: year month day, and Effective cpp item 18
+  Time(int hour, int min, int sec);
+
+  void ComputeNow();
+  uint64_t MilliSeconds() const { return m_ms; }
+  uint64_t MicroSeconds() const { return m_us; }
+  const char* FormatTime(char* buf, int size) const;
+  void AddDelay(uint64_t delay);
+
+  int GetYear() const {
+    _UpdateTm();
+    return m_tm.tm_year + 1900;
+  }
+  int GetMonth() const {
+    _UpdateTm();
+    return m_tm.tm_mon + 1;
+  }
+  int GetDay() const {
+    _UpdateTm();
+    return m_tm.tm_mday;
+  }
+  int GetWDay() const {
+    _UpdateTm();
+    return m_tm.tm_wday;
+  }
+  int GetHour() const {
+    _UpdateTm();
+    return m_tm.tm_hour;
+  }
+  int GetMinute() const {
+    _UpdateTm();
+    return m_tm.tm_min;
+  }
+  int GetSecond() const {
+    _UpdateTm();
+    return m_tm.tm_sec;
+  }
+
+  Time& operator=(const Time&);
+  operator uint64_t() const { return m_ms; }
+
+ private:
+  uint64_t m_ms;  // milliseconds from 1970
+  uint64_t m_us;
+  mutable tm m_tm;
+  mutable bool m_valid;
+
+  void _UpdateTm() const;
+};
+
+extern Time g_now;
+
+class Timer {
+  friend class TimerManager;
+
+ public:
+  explicit Timer(uint32_t interval = uint32_t(-1), int32_t count = -1);
+  virtual ~Timer() {}
+  bool OnTimer();
+  void SetRemainCnt(int32_t remain) { m_count = remain; }
+
+ private:
+  virtual bool _OnTimer() { return false; }
+  Timer* m_next;
+  Timer* m_prev;
+  Time m_triggerTime;
+  uint32_t m_interval;
+  int32_t m_count;
+};
+
+class TimerManager {
+ public:
+  ~TimerManager();
+
+  bool UpdateTimers(const Time& now);
+  void ScheduleAt(Timer* pTimer, const Time& triggerTime);
+  void AddTimer(Timer* pTimer);
+  void AsyncAddTimer(Timer* pTimer);
+  void KillTimer(Timer* pTimer);
+
+  static TimerManager* Instance() {
+    static TimerManager mgr;
+    return &mgr;
+  }
+
+ private:
+  TimerManager() : m_count(0) {}
+
+  bool _Cacsade(Timer pList[], int index);
+  int _Index(int level);
+
+  static const int LIST1_BITS = 8;
+  static const int LIST_BITS = 6;
+  static const int LIST1_SIZE = 1 << LIST1_BITS;
+  static const int LIST_SIZE = 1 << LIST_BITS;
+
+  Time m_lastCheckTime;
+
+  Timer m_list1[LIST1_SIZE];  // 256 ms
+  Timer m_list2[LIST_SIZE];   // 64 * 256ms = 16秒
+  Timer m_list3[LIST_SIZE];   // 64 * 64 * 256ms = 17分钟
+  Timer m_list4[LIST_SIZE];   // 64 * 64 * 64 * 256ms = 18 小时
+  Timer m_list5[LIST_SIZE];   // 64 * 64 * 64 * 64 * 256ms = 49 天
+
+  std::mutex m_lock;  // protect m_timers
+  std::vector<Timer*> m_timers;
+  volatile std::atomic_int m_count;  // count size of m_timers
+};
+
+inline int TimerManager::_Index(int level) {
+  uint64_t current = m_lastCheckTime;
+  current >>= (LIST1_BITS + level * LIST_BITS);
+  return current & (LIST_SIZE - 1);
+}
+
+#endif  // TYLIB_TIMER_TIMER_H_
