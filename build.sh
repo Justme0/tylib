250a59e3cbcc58c6650e7036d9215a9ff651a296
taylorjiang <taylorjiang@tencent.com>
2026-03-03 23:30:24 +0800

    build: Update bazel to 9.0.0 & resolve the shit load cc rules problem

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 set -ex
 
 find tylib | egrep ".+\.(cc|cpp|h)$" | xargs clang-format -i || true
-bazel test --verbose_failures --sandbox_debug --subcommands --explain=bazel_build.log --verbose_explanations --cxxopt="-std=c++17" --test_output=all //:tylib_test
+# Bazel 9 removed native cc rules; autoload them for older BCR packages
+bazel test --incompatible_autoload_externally=cc_library,cc_test,cc_binary --verbose_failures --sandbox_debug --subcommands --explain=bazel_build.log --verbose_explanations --cxxopt="-std=c++17" --test_output=all //:tylib_test
